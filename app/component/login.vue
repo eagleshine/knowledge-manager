1254f7bfde3822ff169ac2c3e51f0075f4ba04e6
Huibing Yin <huibingy@yahoo-inc.com>
2018-02-21 11:09:48 -0600

    Revoked token from server; Still needs to click twice to show email

--- app/component/login.vue
+++ app/component/login.vue
@@ -44,6 +44,7 @@ export default {
       }.bind(this));
     },
     logoutUser: function() {
+      this.revokeToken();
       chrome.storage.local.remove(['profile']);
       this.name = '';
       this.email = '';
@@ -79,6 +80,30 @@ export default {
         });
       }
       getTokenAndXhr();
+    },
+    revokeToken: function() {
+      chrome.identity.getAuthToken({ 'interactive': false }, function(current_token) {
+        if (!chrome.runtime.lastError) {
+          // @corecode_begin removeAndRevokeAuthToken
+          // @corecode_begin removeCachedAuthToken
+          // Remove the local cached token
+          chrome.identity.removeCachedAuthToken({ token: current_token },
+            function() {});
+          // @corecode_end removeCachedAuthToken
+
+          // Make a request to revoke token in the server
+          var xhr = new XMLHttpRequest();
+          xhr.open('GET', 'https://accounts.google.com/o/oauth2/revoke?token=' +
+                  current_token);
+          xhr.send();
+          // @corecode_end removeAndRevokeAuthToken
+
+          // Update the user interface accordingly
+          // $('#revoke').get(0).disabled = true; 
+          console.log('Token revoked and removed from cache. ' +
+            'Check chrome://identity-internals to confirm.');
+        }
+      });
     }
   }
 }
